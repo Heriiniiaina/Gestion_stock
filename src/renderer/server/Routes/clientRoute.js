f849d3f798d8e5715eddb2e3adedfb270a3ec7f6
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-10-14 10:39:08 +0200

    api client

--- src/renderer/server/Routes/clientRoute.js
+++ src/renderer/server/Routes/clientRoute.js
@@ -1,8 +1,10 @@
 import e from "express";
-import { addClient, getAllClient } from "../controllers/clientController.js";
+import { addClient, deleteClient, getAllClient, updateClient } from "../controllers/clientController.js";
 
 const router = e.Router()
 
 router.post("/addClient",addClient)
 router.get("/getAllClient",getAllClient)
+router.delete("/delete/:id",deleteClient)
+router.put("/update/:id",updateClient)
 export default router
--- src/renderer/server/controllers/clientController.js
+++ src/renderer/server/controllers/clientController.js
@@ -46,4 +46,57 @@ export const getAllClient = async(req,res)=>{
             })
         })
 }
+export const deleteClient = async (req,res)=>{
+    const id = req.params.id
+    clientDb.remove({_id:id},{},(err,removed)=>{
+        if(err)
+            return res.status(500).json({
+                success:false,
+                message:"Erreur serveur"
+            })
+        if(removed==0)
+            return res.status(400).json({
+                success:false,
+                message:"Auccun données supprimer"
+            })
+        res.status(200).json({
+            success:true,
+            message:"Client supprimer"
+        })
+    })
+}
 
+export const updateClient = async (req,res)=>{
+    const {nom,prenom,email,telephone} = req.body
+    if(!nom || !prenom || !email || !telephone) 
+        return res.status(400).json({
+            success:false,
+            message:"Veuillez remplir le formulaire"
+        })
+    if(telephone.length !=10)
+        return res.status(400).json({
+            success:false,
+            message:"Numero non valide! Le numero doit avoir exactement 10 caracteres"
+        })
+    if(!validator.isEmail(email))
+        return res.status(400).json({
+            success:false,
+            message:"Email non valide"
+        })
+    const id = req.params.id
+    const updata = {nom,prenom,email,telephone}
+    clientDb.update({_id:id},{$set:updata},{},(err,uptdated)=>{
+            if(err){
+                return res.status(500).json({
+                    success:false,
+                    message:"Erreur serveur"
+
+                })
+            }
+            res.status(200).json({
+                success:true,
+                message:"Client a été mis à jour"
+
+            })
+    })
+}
